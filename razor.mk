e1930c3c26c6403d7a3d4525c03711d1ebdfd38f
vibhu0009 <cvibhu0009@gmail.com>
2015-09-26 09:09:36 +0200

    Make it Razor

--- razor.mk
+++ razor.mk
@@ -1,14 +1,14 @@
 PRODUCT_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 $(call inherit-product, device/lge/d851/full_d851.mk)
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
-$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/razor/config/common_full_phone.mk)
+$(call inherit-product, vendor/razor/config/nfc_enhanced.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 1440
 TARGET_SCREEN_HEIGHT := 2560
 
 PRODUCT_RELEASE_NAME := LG G3
-PRODUCT_NAME := slim_d851
+PRODUCT_NAME := razor_d851
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_DEVICE="g3" PRODUCT_NAME="g3_tmo_us" BUILD_FINGERPRINT="lge/g3_tmo_us/g3:4.4.2/KVT49L.D85110c/D85110c.1403267110:user/release-keys" PRIVATE_BUILD_DESC="g3_tmo_us-user 4.4.2 KVT49L.D85110c D85110c.1403267110 release-keys"
